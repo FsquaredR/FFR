bf6a6ec547c939c8ea8b7e0b075a84f5e85b5451
Tomasz Walczak <tomasz.walczak@live.com>
2012-11-17 19:41:43 -0500

    PI Control - Conflict

--- main_temp.c
+++ main_temp.c
@@ -33,26 +33,21 @@
 #define FOSC 8000000
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD-1
-#define SAMPLERATE 10
+#define SAMPLERATE 40
 
-/* Encoder Varriables*/
-short leftDirection = 0;	//1 = forward : -1 = Reverse
-int count_LeftFWD = 0;
-int count_LeftRVS = 0;
-short rightDirection = 0;	//1 = forward : -1 = Reverse
-int count_RightFWD = 0;
-int count_RightRVS = 0;
+/* Encoders */
+short dirL = 0;
+volatile uint16_t countLF = 0;
+volatile uint16_t countLR = 0;
+short dirR = 0;
+volatile uint16_t countRF = 0;
+volatile uint16_t countRR = 0;
 
-/* Prototypes*/
+/* PROTOTYPES */
 void motorSTP(void);
-
-int ping_cm(void);
-long pingRead(void);
-
 long readIR(int);
 int readADC(int);
-
-
+int readADC2(int);
 unsigned char USART_Receive(void);
 void USART_TransmitString(const char* str);
 void USART_Transmit(unsigned char data);
@@ -61,8 +56,11 @@ void USART_Init(unsigned int ubrr);
 volatile uint16_t adc_value;
 volatile uint8_t  bool_adc_done;
 volatile uint8_t  bool_led;
+volatile uint8_t  bool_pid;
 
-volatile uint8_t enc_count, left_speed;
+volatile uint16_t enc_count;
+volatile int16_t left_speed;
+volatile int16_t right_speed;
 
 																
 void init(void){
@@ -96,183 +94,6 @@ void init(void){
 	bool_led = 1;
 }
 
-int main(void)
-{
-    init();
-    
-    USART_Init(MYUBRR);
-
-    sei();
-    LED(0);
-    USART_TransmitString((char*)"Micro Started: Push Button\n");
-    while(PBS)
-    {
-        _delay_ms(1);
-    }
-    LED(1);
-    USART_TransmitString((char*)"Button Pushed\n");
-    uint8_t i = 0;
-    char buffer[10];
-	while(1){
-		memset(buffer,0,10);
-		ATOMIC_BLOCK(ATOMIC_FORCEON) {itoa(left_speed, buffer, 10);}
-		OCR0A = 50;
-		OCR0B = 50;
-		if(!PBS) {
-			motorFWD();
-		} else {
-			motorSTP();
-		}
-		USART_TransmitString(buffer);
-		USART_Transmit('\n');
-		_delay_ms(250);
-    }
-}
-
-
-
-uint16_t ping_block()
-{
-
-}
-
-/* Motor Control Functions */ //Funky needs checking
-void setMotorSpeed(char c, int p){		// Set speed with a percentage
-	int s = 0;
-	
-	if(p >= 0 && p <= 100){ 
-		s = (p * 255)/100;
-	}	
-	
-	if(c == 'L') OCR1A = s;				// PORTD 5
-	if(c == 'R') OCR1B = s;				// PORTD 4
-}
-
-/*----------PING Sensor-----------*/
-int ping_cm(){
-   long avg = 0;
-   long sum = 0;
-  
-  for(int i = 0; i < (SAMPLERATE); i++){
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-		sum += pingRead();
-		_delay_ms(1);	
-	}    
-  }
-  avg = (sum/(SAMPLERATE));
-
-  return (avg /29 /2);		// Needed to be recalibrated... maybe... probably not though
-}
-
- long pingRead(){	
-	long count = 0;
-
-	ULTRA_OUT;             
-	ULTRA_WRITE(0);				    // Pulse The Ping Sensor 
-	_delay_us(2);
-	ULTRA_WRITE(1);
-	_delay_us(5);
-	ULTRA_WRITE(0);
-	ULTRA_IN;
-
-	while(!ULTRA_READ){}
-	while(ULTRA_READ){
-		count++;
-		_delay_us(1);
-	}
-	return count;  
-}
-
-/*----------------ADC Functions-------------*/
-int readADC(int channel){		// PORT A
-	bool_adc_done = 0;
-	ADMUX  &= 0xE0;
-	ADMUX  |= channel;
-	ADCSRA |= (1 << ADIE);
-	ADCSRA |= (1 << ADSC);
-	while(bool_adc_done==0) asm("nop"); //Block till conversion's done
-	return adc_value;
-}
-
-/*----------Interrupts------------*/
-ISR(INT0_vect){
-    if(LENC0){
-<<<<<<< HEAD
-        if(LENC1){          //Checks other right encoder
-            leftDirection = 1;
-            count_LeftFWD++;
-        }else{
-            leftDirection = -1;
-            count_LeftRVS++;
-=======
-        if(!LENC1){          //Checks other right encoder
-            //dirL = 1;
-            //countLF++;
-            enc_count++;
-        }else{
-            //dirL = -1;
-            //countLR++;
->>>>>>> Starting PI controller
-        }
-    }
-    
-}
-
-ISR(INT1_vect){
-    if(RENC0){
-        if(!RENC1){          //Checks other right encoder
-<<<<<<< HEAD
-            rightDirection = 1;
-            count_RightFWD++;
-        }else{
-            rightDirection = -1;
-            count_RightRVS++;
-=======
-            dirR = 1;
-            LED(0);
-            countRF++;
-        }else{
-            dirR = -1;
-            LED(1);
-            countRR++;
->>>>>>> Starting PI controller
-        }
-    }
-}
-
-ISR(ADC_vect){ 
-	adc_value = ADC;
-	ADCSRA &= ~(1<<ADSC); // TURN OFF ADC
-	ADCSRA &= ~(1<<ADIE);
-	bool_adc_done = 1;
-} 
-
-<<<<<<< HEAD
-ISR(TIMER1_OVF_vect){ 
-	if(bool_led) {
-		LED(1);
-		bool_led = 0;
-	} else {
-		LED(0);
-		bool_led = 1;
-	}
-=======
-ISR(TIMER1_OVF_vect) { 
-	//if(bool_led) {
-		//LED(1);
-	//	bool_led = 0;
-	//} else {
-		//LED(0);
-	//	bool_led = 1;
-	//}
-	left_speed = enc_count;
-	enc_count = 0;
-   
->>>>>>> Starting PI controller
-   TCNT1  = 49911; // Reload timer with precalculated value 
-}
-
-/*----------Serial Comm------------*/
 void USART_TransmitString(const char* str)
 {
 	for (;*str;str++)
@@ -313,27 +134,186 @@ void USART_Init(unsigned int ubrr)
 	
 }
 
-/*----------Motor Control------------*/
-void motorFWD(void){				
-	LM0(1);
-	LM1(0);
-	LM1(0);
-	RM0(0);
-	RM1(1);	
+
+int main(void)
+{
+    init();
+    
+    USART_Init(MYUBRR);
+
+    sei();
+    LED(0);
+    USART_TransmitString((char*)"Micro Started: Push Button\n");
+    while(PBS)
+    {
+        _delay_ms(1);
+    }
+    LED(1);
+    USART_TransmitString((char*)"Button Pushed\n");
+    motorFWD();
+    uint8_t i = 0;
+    char buffer[10];
+	while(1){
+		memset(buffer,0,10);
+		itoa(left_speed, buffer, 10);
+		USART_TransmitString(buffer);
+		//USART_Transmit(',');
+		//itoa(right_speed, buffer, 10);
+		//USART_TransmitString(buffer);
+		//OCR0A = 100;
+		//OCR0B = 50;
+		if(!PBS) {
+			OCR0A = 100;
+			OCR0B = 100;
+		} else {
+			OCR0A = 0;
+			OCR0B = 0;
+		}
+		
+		USART_Transmit('\n');
+		_delay_ms(50);
+    }
 }
 
-void motorRVS(void){				
-	LM0(0);
-	LM1(1);
-	RM0(1);
-	RM1(0);	
+
+
+uint16_t ping_block()
+{
+
 }
 
-void motorSTP(){
-	LM0(0);
-	LM1(0);
-	RM0(0);
-	RM1(0);
+/* Motor Control Functions */ //Funky needs checking
+void setMotorSpeed(char c, int p){		// Set speed with a percentage
+	int s = 0;
+	
+	if(p >= 0 && p <= 100){ 
+		s = (p * 255)/100;
+	}	
+	
+	if(c == 'L') OCR1A = s;				// PORTD 5
+	if(c == 'R') OCR1B = s;				// PORTD 4
+}
+
+/* PING Functions */ // Needs to be tested
+/*
+int ping_cm(){
+  int avg = 0;
+  int sum = 0;
+  
+  for(int i = 0; i < (SAMPLERATE/2); i++){
+    sum += pingRead();
+    _delay_ms(5);	
+  }
+  avg = (sum/(SAMPLERATE/2));
+  
+  return (avg /29 /2);		// Needed to be recalabrated... maybe... probably not though
+}
+
+int pingRead(){
+  int onTime = 0;
+  
+  FLIPBIT(PORTC,8);             
+  ULTRAW(0);				    // Pulse The Ping Sensor 
+  _delay_us(2);
+  ULTRAW(1);
+  _delay_us(5);
+  ULTRAW(0);
+  FLIPBIT(PORTC,8);
+  
+  onTime = pulseInHighUltra();	// Collect Data 
+  
+  return onTime;  
+}
+
+int pulseInHighUltra(){
+	int count = 0;
+	
+	while(ULTRAR){ 					//Pulse is high
+		count++;
+		_delay_us(1);
+	}
+	
+	 return count;
+}
+*/
+ISR(INT0_vect){
+    if(LENC0){
+        if(LENC1){          //Checks other right encoder
+            dirL = 1;
+            LED(0);
+            countLF++;
+        }else{
+            dirL = -1;
+            LED(1);
+            countLR++;
+        }
+    }  
+}
+
+ISR(INT1_vect){
+    if(RENC0){
+        if(!RENC1){          //Checks other right encoder
+            dirR = 1;
+            countRF++;
+        }else{
+            dirR = -1;
+            countRR++;
+        }
+    }
+}
+ISR(ADC_vect) 
+{ 
+	adc_value = ADC;
+	ADCSRA &= ~(1<<ADSC); // TURN OFF ADC
+	ADCSRA &= ~(1<<ADIE);
+	bool_adc_done = 1;
+} 
+
+ISR(TIMER1_OVF_vect) { 
+	//if(bool_led) {
+		//LED(1);
+	//	bool_led = 0;
+	//} else {
+		//LED(0);
+	//	bool_led = 1;
+	//}
+	left_speed = countRF;
+	right_speed = countLF;
+	countRF = 0;
+	countLF = 0;
+
+	/*if(bool_pid){
+		if(left_speed < 200){
+			OCR0A++;
+		} else if(left_speed > 200) {
+			OCR0A--;
+		}
+	} else {
+		if(left_speed < 50){
+			OCR0A++;
+
+		} else if(left_speed > 50) {
+			OCR0A--;
+		}
+	}
+   
+	if(bool_pid){
+		if(right_speed < 200){
+			OCR0B++;
+		} else if(right_speed > 200) {
+			OCR0B--;
+		}
+	} else {
+		if(right_speed < 50){
+			OCR0B++;
+
+		} else if(right_speed > 50) {
+			OCR0B--;
+		}
+	}*/
+
+
+   TCNT1  = 49911; // Reload timer with precalculated value 
 }
 /*
 void go(int d, short dirR, short dirL){			// Assuming overall encoders get cleared ( d in increments )
@@ -401,9 +381,6 @@ void go(int d, short dirR, short dirL){			// Assuming overall encoders get clear
             break;
     }
     return -1; // RETURN WITH ERROR
-<<<<<<< HEAD
-}/**/
-=======
 }/**/
 
 /* ADC Functions */
@@ -445,4 +422,3 @@ void motorSTP(){
 	RM0(0);
 	RM1(0);
 }
->>>>>>> Starting PI controller
